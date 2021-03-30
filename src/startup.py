014ff29bfeebad3f473680b3cb4fee9cc3e7f2fb
tys-hiroshi <tashiro.hiroshi@toyoko-sys.co.jp>
2021-03-31 08:48:15 +0900

    redoc http://127.0.0.1:8001/api/v1/redoc

--- src/startup.py
+++ src/startup.py
@@ -1,12 +1,55 @@
-# import uvicorn
 from fastapi import FastAPI
+from fastapi.openapi.utils import get_openapi
 
-app = FastAPI()
+# 各タグの説明
+tags_metadata = [
+    {
+        "name": "users",
+        "description": "Operations with users. The **login** logic is also here.",
+    },
+    {
+        "name": "items",
+        "description": "Manage items. So _fancy_ they have their own docs.",
+        "externalDocs": {
+            "description": "Items external docs",
+            "url": "https://fastapi.tiangolo.com/",
+        },
+    },
+]
+
+app = FastAPI(
+    title="FastAPI Sample Project",
+    description="This is a very fancy project, with auto docs for the API and everything",
+    version="1.0.1",
+    openapi_tags=tags_metadata,
+    docs_url="/api/v1/docs",
+    redoc_url="/api/v1/redoc",
+    openapi_url="/api/v1/openapi.json",
+    )
 
 
-@app.get("/hello")
-def hello():
-    return {"Hello": "World!"}
+@app.get(
+    "/items/",
+    tags=["items"],
+    )
+async def read_items():
+    return [{"name": "Foo"}]
 
-# if __name__ == "__main__":
-#     uvicorn.run(app, host="0.0.0.0", port=8001)
+
+def custom_openapi():
+    if app.openapi_schema:
+        return app.openapi_schema
+    openapi_schema = get_openapi(
+        title="Custom title",
+        version="2.5.0",
+        description="This is a very custom OpenAPI schema",
+        routes=app.routes,
+    )
+    openapi_schema["info"]["x-logo"] = {
+        "url": "https://fastapi.tiangolo.com/img/logo-margin/logo-teal.png"
+    }
+    app.openapi_schema = openapi_schema
+    return app.openapi_schema
+
+
+app.openapi = custom_openapi
