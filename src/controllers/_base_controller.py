d099d2d3d90908faf1b36f2450da40eb5a69ca6c
tys-hiroshi <tashiro.hiroshi@toyoko-sys.co.jp>
2021-04-20 12:32:31 +0900

    add base controller

---
+++ src/controllers/_base_controller.py
@@ -0,0 +1,59 @@
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.openapi.utils import get_openapi
+from fastapi.templating import Jinja2Templates
+
+app = FastAPI(
+    title='base fastapi template',
+    description='base fastapi template',
+    version='0.0.0'
+)
+
+# 各タグの説明
+tags_metadata = [
+    {
+        "name": "users",
+        "description": "Operations with users. The **login** logic is also here.",
+    },
+    {
+        "name": "items",
+        "description": "Manage items. So _fancy_ they have their own docs.",
+        "externalDocs": {
+            "description": "Items external docs",
+            "url": "https://fastapi.tiangolo.com/",
+        },
+    },
+]
+
+app = FastAPI(
+    title="FastAPI Sample Project",
+    description="This is a very fancy project, with auto docs for the API and everything",
+    version="1.0.1",
+    openapi_tags=tags_metadata,
+    docs_url="/api/v1/docs",
+    redoc_url="/api/v1/redoc",
+    openapi_url="/api/v1/openapi.json",
+    )
+
+templates = Jinja2Templates(directory="src/templates")
+
+app.mount("/static", StaticFiles(directory="src/static"), name="static")
+
+@app.get(
+    "/index",
+    tags=["root"],
+    response_class=HTMLResponse
+    )
+async def index(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request, "id": 1})
+
+@app.get(
+    "/admin",
+    tags=["admin"],
+    response_class=HTMLResponse
+    )
+async def admin(request: Request):
+    return templates.TemplateResponse('admin.html',
+                                    {'request': request,
+                                    'username': 'admin'})
