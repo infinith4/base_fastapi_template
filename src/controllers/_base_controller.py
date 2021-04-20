2cbc81cfc01411c3db94b9f437179d2d9eb9bc2a
tys-hiroshi <tashiro.hiroshi@toyoko-sys.co.jp>
2021-04-21 07:28:53 +0900

    mod settings

--- src/controllers/_base_controller.py
+++ src/controllers/_base_controller.py
@@ -4,6 +4,21 @@ from fastapi.staticfiles import StaticFiles
 from fastapi.openapi.utils import get_openapi
 from fastapi.templating import Jinja2Templates
 
+def custom_openapi():
+    if app.openapi_schema:
+        return app.openapi_schema
+    openapi_schema = get_openapi(
+        title='base fastapi template',
+        description='description fastapi template',
+        version='0.0.0',
+        routes=app.routes,
+    )
+    openapi_schema["info"]["x-logo"] = {
+        "url": "https://fastapi.tiangolo.com/img/logo-margin/logo-teal.png"
+    }
+    app.openapi_schema = openapi_schema
+    return app.openapi_schema
+
 # 各タグの説明
 tags_metadata = [
     {
@@ -21,14 +36,14 @@ tags_metadata = [
 ]
 
 app = FastAPI(
-    title='base fastapi template',
-    description='base fastapi template',
-    version='0.0.0',
     openapi_tags=tags_metadata,
     docs_url="/api/v1/docs",
     redoc_url="/api/v1/redoc",
     openapi_url="/api/v1/openapi.json",
     )
+
+
+app.openapi = custom_openapi
 
 templates = Jinja2Templates(directory="src/templates")
 
--- src/urls.py
+++ src/urls.py
@@ -3,23 +3,6 @@ from controllers.index_controller import *
 from controllers.admin_controller import *
 from controllers.user_controller import *
 
-def custom_openapi():
-    if app.openapi_schema:
-        return app.openapi_schema
-    openapi_schema = get_openapi(
-        title="Custom title",
-        version="2.5.0",
-        description="This is a very custom OpenAPI schema",
-        routes=app.routes,
-    )
-    openapi_schema["info"]["x-logo"] = {
-        "url": "https://fastapi.tiangolo.com/img/logo-margin/logo-teal.png"
-    }
-    app.openapi_schema = openapi_schema
-    return app.openapi_schema
-
-app.openapi = custom_openapi
-
 # FastAPIのルーティング用関数
 app.add_api_route('/', index)
 app.add_api_route('/admin', admin)  # management view for administrator
